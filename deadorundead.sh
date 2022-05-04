931799839ec175d2ad7fd69705bcb7364d8ff68d
kingbonj <38471159+kingbonj@users.noreply.github.com>
2022-05-04 09:31:18 +0100

    Update deadorundead.sh

--- deadorundead.sh
+++ deadorundead.sh
@@ -1,26 +1,42 @@
 #!/bin/sh
 
+#Colours
+
+GREEN='\e[0;32m'
+RED='\e[0;31m'
+NC='\e[0m' # No Color
+BOLD='\e[1m'
+DIM='\e[2m'
+BLINK='\e[5m'
+
     echo "Is George A. Romero still dead?"
     echo "Checking Wikipedia/George_A._Romero for information..."
 if curl -s https://en.wikipedia.org/wiki/George_A._Romero | grep -q Died; then
     echo ""
-    echo "                          RIP George A. Romero (1940-2017)"
-    echo "                                 Still dead for now..."
+    echo "                          $GREEN$BOLD  RIP George A. Romero (1940-2017)$NC"
+    echo "                                 $RED Still dead for now...$NC"
     echo ""
-    echo "Learn more about this great man:"
+    echo "                            Learn more about this great man:"
     echo ""
-    echo "      https://en.wikipedia.org/wiki/George_A._Romero"
-    echo "      https://en.wikipedia.org/wiki/George_A._Romero_filmography"
-    echo "      http://www.officialgeorgeromero.com"
-    echo "      https://romero.library.pitt.edu"
-    echo "      https://georgearomerofoundation.org"
-    echo "      https://www.findagrave.com/memorial/181411973/george-romero"
-    echo ""
-    echo "Stay Scared!"
-    echo ""    
+    echo "                      https://en.wikipedia.org/wiki/George_A._Romero"
+    echo "                https://en.wikipedia.org/wiki/George_A._Romero_filmography"
+    echo "                          http://www.officialgeorgeromero.com"
+    echo "                            https://romero.library.pitt.edu"
+    echo "                          https://georgearomerofoundation.org"
+    echo "                https://www.findagrave.com/memorial/181411973/george-romero"
+    echo "$RED"
+echo "                                     Stay Scared!"
+    echo "$NC"    
 else
-    echo "There is no more room in hell..."
-    echo "The dead are walking the earth..."
-    echo "https://www.wikihow.com/Survive-an-Apocalypse"
-    echo "Good Luck!"
+DATE=$(date +"%Y")
+    echo ""
+    echo "                          $GREEN$BOLD  George A. Romero (1940-$DATE)$NC"
+    echo "$RED$BLINK"
+    echo "                           THERE IS NO MORE ROOM IN HELL"
+    echo "                                  THE DEAD WALK!$NC"
+    echo ""
+    echo "                      https://www.wikihow.com/Survive-an-Apocalypse"
+    echo "$RED"
+    echo "                                      Good Luck!"
+    echo ""
 fi
